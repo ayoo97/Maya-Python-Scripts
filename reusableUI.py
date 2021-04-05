967ecc3d9ad67efee0ea086e6246a8fc040539fd
Alex <ayoo97@gmail.com>
2021-04-05 16:40:26 -0700

    added comments

--- reusableUI.py
+++ reusableUI.py
@@ -29,27 +29,6 @@ class BaseWindow:
     def close(self, *args):
         cmds.deleteUI(self.windowName)
 
-class TweenerUI(BaseWindow):
-
-    windowName = "TweenerWindow"
-
-    def buildUI(self):
-        column = cmds.columnLayout()
-
-        cmds.text(label = "Use the slider to set the tween amount")
-
-        row = cmds.rowLayout(numberOfColumns = 2)
-
-        self.slider = cmds.floatSlider(min = 0, max = 100, value = 50, step = 1, changeCommand = tween)
-
-        cmds.button(label = "Reset", command = self.reset)
-
-        cmds.setParent(column)
-        cmds.button(label = "Close", command = self.close)
-
-    def reset(self, *args):
-        cmds.floatSlider(self.slider, edit = True, value = 50)
-
 class GearUI(BaseWindow):
 
     windowName = "GearWindow"
@@ -58,24 +37,34 @@ class GearUI(BaseWindow):
         self.gear = None
 
     def buildUI(self):
-
+        
+        # top most window layout - this means the entire tool window is a single "column"
         column = cmds.columnLayout()
         cmds.text(label = "Use the sliders to modify the gear")
 
+        # first row layout with 4 columns inside it for the following: label, slider, slider number, reset button
         cmds.rowLayout(numberOfColumns = 4)
         cmds.text(label = "Teeth   ")
+        # creates a slider tool to control the number of teeth (defaults the slider and label to 10)
         self.teethSlider = cmds.intSlider(min = 5, max = 30, value = 10, step = 1, dragCommand = self.modifyTeeth)
         self.teethSliderLabel = cmds.text(label = "10")
         cmds.button(label = "Reset", command = self.resetTeeth)
+        # set the "column" which is the top most layout as its container
+        # this allows for another layout to be created and marks the end of the current layout
         cmds.setParent(column)
 
+        # second row layout with 4 columns inside it for the following: label, slider, slider number, reset button
         cmds.rowLayout(numberOfColumns = 4)
         cmds.text(label = "Length ")
+        # creates a slider tool to control the length of each tooth (defaults the slider and label to 0.3)
         self.lengthSlider = cmds.floatSlider(min = 0.1, max = 1.0, value = 0.3, step = 0.1, dragCommand = self.modifyLength)
         self.lengthSliderLabel = cmds.text(label = "0.3")
         cmds.button(label = "Reset", command = self.resetLength)
+        # set the "column" which is the top most layout as its container
+        # this allows for another layout to be created and marks the end of the current layout
         cmds.setParent(column)
 
+        # third row layout with 2 columns inside it for the following: make gear button, close button
         cmds.rowLayout(numberOfColumns = 2)
         cmds.button(label = "Make Gear", command = self.makeGear)
         cmds.button(label = "Close", command = self.close)
@@ -106,3 +95,24 @@ class GearUI(BaseWindow):
         cmds.floatSlider(self.lengthSlider, edit = True, value = 0.3)
         cmds.text(self.lengthSliderLabel, edit = True, label = "0.3")
         self.gear.changeTeeth(teeth = self.gear.teeth)
+
+class TweenerUI(BaseWindow):
+
+    windowName = "TweenerWindow"
+
+    def buildUI(self):
+        column = cmds.columnLayout()
+
+        cmds.text(label = "Use the slider to set the tween amount")
+
+        row = cmds.rowLayout(numberOfColumns = 2)
+
+        self.slider = cmds.floatSlider(min = 0, max = 100, value = 50, step = 1, changeCommand = tween)
+
+        cmds.button(label = "Reset", command = self.reset)
+
+        cmds.setParent(column)
+        cmds.button(label = "Close", command = self.close)
+
+    def reset(self, *args):
+        cmds.floatSlider(self.slider, edit = True, value = 50)
